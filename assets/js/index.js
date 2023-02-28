ad5227edc299fe911c34a395c08cf0f80fb17741
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-02-28 20:51:14 +0800

    Add query selectors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,18 @@
 const buttons = document.querySelectorAll(`button[data-btn]`);
 const currentResult = document.querySelector("#currentResult");
 
+const add = document.querySelector("#sum");
+const sub = document.querySelector("#difference");
+const multiply = document.querySelector("#product");
+const divide = document.querySelector("#quotient");
+const calculate = document.querySelector("#calculate");
+
+const dataCollector = [];
+let result = null;
 
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         if (!e.target.dataset.btn) return;
-
-        currentResult.textContent += e.target.dataset.btn;
-    })
-})
+        currentResult.textContent += parseInt(e.target.dataset.btn);
+    });
+});
